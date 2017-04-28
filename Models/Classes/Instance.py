63bca218c4fabbc73c86487faeb28e3ea55fd2e9
Piotr Pawelczyk <piopawelczyk@gmail.com>
2017-04-28 11:23:42 +0200

    [TASK3] Stworzenie modeli obiektów ImagingStudy i ImagingManifest Added Classes ImagingStudy and Imaging Manifest. Those classes use another classes such as Series, Study and Instance

---
+++ Models/Classes/Instance.py
@@ -0,0 +1,6 @@
+class Instance:
+    def __init__(self, parameters):
+        self.uid = parameters[0]
+        self.number = parameters[1]
+        self.sopClass = parameters[2]
+        self.title = parameters[3]
