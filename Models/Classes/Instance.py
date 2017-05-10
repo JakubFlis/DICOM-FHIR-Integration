d7b5458146062afcb2c1667336bfe7838a5ec67b
Piotr Pawelczyk <piopawelczyk@gmail.com>
2017-05-10 23:22:30 +0200

    Cleared some commit issues with Instance, Series and ImagingStudy classes

--- Models/Classes/Instance.py
+++ Models/Classes/Instance.py
@@ -7,7 +7,6 @@ instance = Instance(instanceParams)
 """
 class Instance:
     def __init__(self, parameters):
-<<<<<<< HEAD
          self.uid = parameters[INSTANCE.UID]
          self.number = parameters[INSTANCE.NUMBER]
          self.sopClass = parameters[INSTANCE.SOPCLASS]
@@ -18,9 +17,4 @@ class INSTANCE(object):
     NUMBER = 1
     SOPCLASS = 2
     TITLE = 3
-=======
-        self.uid = parameters[0]
-        self.number = parameters[1]
-        self.sopClass = parameters[2]
-        self.title = parameters[3]
->>>>>>> 627ce2dc19d87e382562bee373981a0f4ba75d00
+
--- Models/Classes/Series.py
+++ Models/Classes/Series.py
@@ -9,7 +9,6 @@ series = Series(seriesParams, instance)
 """
 class Series:
     def __init__(self, parameters, instance):
-<<<<<<< HEAD
         self.uid = parameters[SERIES.UID]
         self.number = parameters[SERIES.NUMBER]
         self.modality = parameters[SERIES.MODALITY]
@@ -35,17 +34,3 @@ class SERIES(object):
     LATERALITY = 8
     STARTED = 9
     PERFORMER = 10
-=======
-        self.uid = parameters[0]
-        self.number = parameters[1]
-        self.modality = parameters[2]
-        self.description = parameters[3]
-        self.numberOfInstances = parameters[4]
-        self.availability = parameters[5]
-        self.endpoint = parameters[6]
-        self.bodySite = parameters[7]
-        self.laterality = parameters[8]
-        self.started = parameters[9]
-        self.performer = parameters[10]
-        self.instance = instance
->>>>>>> 627ce2dc19d87e382562bee373981a0f4ba75d00
